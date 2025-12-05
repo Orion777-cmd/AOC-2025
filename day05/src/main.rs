5c7506ed89570c13c7348a55e44d1af7a90c3e0c
orion777-cmd <abiy.biru78@gmail.com>
2025-12-05 04:28:20 -0800

    add rust solution for day 5 part 2

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -6,7 +6,56 @@ fn main() {
         .lines()
         .map(String::from)
         .collect();
-    
+
+    let res = part_one(&lines);
+
+    println!("first solution: {res}");
+
+    let res_two = part_two(&lines);
+
+    println!("second solution: {res_two}");
+}
+
+fn part_two(lines: &Vec<String>) -> i64 {
+    let mut ranges: Vec<Vec<i64>> = Vec::new();
+    let mut res = 0;
+
+    for line in lines {
+        if line.is_empty() {
+            break;
+        }
+        let nums: Vec<i64> = line
+            .split('-')
+            .map(|x| x.parse().unwrap())
+            .collect();
+        ranges.push(nums);
+    }
+
+    ranges.sort();
+
+    let mut merged: Vec<Vec<i64>> = Vec::new();
+    merged.push(ranges[0].clone());
+
+    for i in 1..ranges.len() {
+        let last = merged.last_mut().unwrap();
+
+        if ranges[i][0] > last[1] {
+            merged.push(ranges[i].clone());
+        } else {
+            last[1] = last[1].max(ranges[i][1]);
+        }
+    }
+
+    println!("{merged:?}");
+
+    for range in merged {
+        res += range[1] - range[0] + 1;
+    }
+
+    res
+}
+
+fn part_one(lines: &Vec<String>) -> i64 {
     let mut ranges: Vec<Vec<i64>> = Vec::new();
     let mut res = 0;
 
@@ -32,6 +81,7 @@ fn main() {
             }
         }
     }
+    
+    res
 
-    println!("{res}");
 }
