ed87100f6efba9448e2075ece1818f50ad2572c1
orion777-cmd <abiy.biru78@gmail.com>
2025-12-11 02:23:11 -0800

    add rust solution for day 11 part 2

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -14,7 +14,10 @@ fn main() {
         let splited: Vec<&str> = line.split(':').collect();
 
         let key = splited[0].trim();
-        let values: Vec<&str> = splited[1].split_whitespace().collect();
+        let values: Vec<&str> = splited[1]
+            .split_whitespace()
+            .map(|v| v.trim())
+            .collect();
 
         graph.insert(key, values);
     }
@@ -22,9 +25,13 @@ fn main() {
     let res_one = part_one(&graph);
 
     println!("the number of paths towards the output is => {res_one}");
+
+    let res_two = part_two(&graph);
+
+    println!("The number of paths pasing through fft and dac is => {res_two}");
 }
 
-fn part_one(graph: &HashMap<&str, Vec<&str>>) -> u32 {
+fn part_one(graph: &HashMap<&str, Vec<&str>>) -> u64 {
     let mut paths = 0;
 
     let mut queue: VecDeque<&str> = VecDeque::new();
@@ -44,3 +51,51 @@ fn part_one(graph: &HashMap<&str, Vec<&str>>) -> u32 {
 
     paths
 }
+
+fn dfs(
+    start: &str,
+    end: &str,
+    graph: &HashMap<&str, Vec<&str>>,
+    memo: &mut HashMap<String, u64>,
+) -> u64 {
+    if start == end {
+        return 1
+    }
+
+    if let Some(val) = memo.get(start) {
+        return *val
+    }
+
+    let mut res = 0;
+    if let Some(neighbors) = graph.get(start) {
+        for  &neigh in neighbors {
+            res += dfs(neigh, end, graph, memo);
+        }
+    }
+
+    memo.insert(start.to_string(), res);
+
+    res
+}
+
+
+fn part_two(graph: &HashMap<&str, Vec<&str>>) -> u64 {
+    let mut memo1 = HashMap::new();
+    let mut memo2 = HashMap::new();
+    let mut memo3 = HashMap::new();
+    let mut memo4 = HashMap::new();
+    let mut memo5 = HashMap::new();
+    let mut memo6 = HashMap::new();
+
+    let p1 = dfs("svr", "dac", graph, &mut memo1)
+           * dfs("dac", "fft", graph, &mut memo2)
+           * dfs("fft", "out", graph, &mut memo3);
+
+    let p2 = dfs("svr", "fft", graph, &mut memo4)
+           * dfs("fft", "dac", graph, &mut memo5)
+           * dfs("dac", "out", graph, &mut memo6);
+
+    p1 + p2
+}
+
+
